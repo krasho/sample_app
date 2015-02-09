c0040c99b2e3056ffc5f6686c8c1a6db62462cd9
Jose Luis <tigrillo34@hotmail.com>
2015-02-09 14:26:09 -0500

    adding email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
-	validates :name, presence: true
-	validates :email, presence: true
+class User < ActiveRecord::Base	
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 }
 end
