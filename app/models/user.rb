0a22c44d8385f80057371e7f7a2a6895bed7d0a5
Krasho <joseluis@balidevelop.com>
2015-02-08 23:36:58 -0500

    Adding field password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
 	validates :name, presence:  true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 }
 
@@ -6,5 +7,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }	
+    
 
+    has_secure_password
 end
