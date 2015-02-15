a6d61edcb77296815d0e2210a44410df79b18734
Krasho <joseluis@balidevelop.com>
2015-02-15 12:04:11 -0500

    signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user =  User.new(params[:user])
-
+  	@user =  User.new(user_params)
   	if @user.save
   		#Handle Succesful save
   	else
   		render 'new'
   	end
   end
+
+  private # Private methods
+
+	  def user_params
+	     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
 end
