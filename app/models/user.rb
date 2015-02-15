5ee864d1342b55346c41c0d3eb631c895f74b782
Krasho <joseluis@balidevelop.com>
2015-02-14 21:50:33 -0500

    User validations finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }	
 
-    validates :password, length: {minimun: 6} 
     
-
     has_secure_password
+    validates :password, length: { minimum: 6 }
 end
